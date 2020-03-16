2c217882b4b0a0a91351401a20a090c7a23b94c3
willis12rus <49200813+willis12rus@users.noreply.github.com>
2020-03-16 12:21:15 +0300

    fix errors with code(tests are not fixed)

--- lab1/Replace/Replace.cpp
+++ lab1/Replace/Replace.cpp
@@ -34,15 +34,23 @@ string ReplaceString(const string& subject, const string& searchString, const st
 
 void CopyTextWithReplace(istream& input, ostream& output, const string& searchString, const string& replaceString)
 {
-	if (searchString.empty())
-	{
-		return;
-	}
 	string line;
 	while (getline(input, line))
 	{
 		output << ReplaceString(line, searchString, replaceString) << "\n";
 	}
+}
+
+int OpenFiles(char* input, char* output, ifstream& fileIn, ofstream& fileOut)
+{
+	fileIn.open(input);
+	fileOut.open(output);
+	
+	if (!fileIn.is_open() || !fileOut.is_open()){
+		cout << "Can't find input/output file\n"
+			 << "Please, enter correct file names\n";
+		return 1;
+	}  
 }
 
 int main(int argc, char* argv[])
@@ -54,22 +62,13 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	ifstream fileIn;
-	fileIn.open(argv[1]);
-
-	ofstream fileOut;
-	fileOut.open(argv[2]);
-	
-	if (!fileIn.is_open() || !fileOut.is_open()){
-		cout << "Can't find input/output file\n"
-			 << "Please, enter correct file names\n";
-		return 1;
-	}
-	
+	ifstream inputFile;
+	ofstream outputFile;
+	OpenFiles(argv[1], argv[2], inputFile, outputFile);
 	string search = argv[3];
 	string replace = argv[4];
-	CopyTextWithReplace(fileIn, fileOut, search, replace);
-	if (!fileOut.flush())
+	CopyTextWithReplace(inputFile, outputFile, search, replace);
+	if (!outputFile.flush())
 	{
 		cout << "Error with writing in output file: please check buffer\n";
 		return 1;
